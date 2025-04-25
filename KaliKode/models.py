3499c246b74edf5039c18d9c058ba336e960a22e
eduda-agc <137100218+eduda-agc@users.noreply.github.com>
2025-04-24 22:53:02 -0300

    consertando comentários

--- KaliKode/models.py
+++ KaliKode/models.py
@@ -3,27 +3,26 @@ from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
 import hashlib
 
-# Inicializa o SQLAlchemy
 db = SQLAlchemy()
 
-# Função simples de "criptografia reversível" (para exemplo)
+# criptografia simples
 def criptografar_senha(senha):
     return hashlib.sha256(senha.encode()).hexdigest()
 
 class Editor(db.Model):
     __tablename__ = 'editores'
-    id = db.Column(db.String(2), primary_key=True)  # Hexadecimal de 00 a FF
+    id = db.Column(db.String(2), primary_key=True)  # contagem hex de 00 a FF
     nome = db.Column(db.String(50), unique=True, nullable=False)
-    senha = db.Column(db.String(64), nullable=False)  # Hash SHA-256
+    senha = db.Column(db.String(64), nullable=False) 
 
     riddles = relationship('Riddle', back_populates='criador')
 
 class Riddle(db.Model):
     __tablename__ = 'riddles'
-    id = db.Column(db.String(3), primary_key=True)  # Hexadecimal de 000 a FFF
+    id = db.Column(db.String(3), primary_key=True)  # contagem hex de 00 a FF
     id_usuario = db.Column(db.String(2), ForeignKey('editores.id'), nullable=False)
     string1 = db.Column(db.String(50), nullable=False)
-    imagem = db.Column(db.String(100), nullable=False)  # Caminho da imagem
+    imagem = db.Column(db.String(100), nullable=False)  # caminho da imagem
     string2 = db.Column(db.String(50), nullable=False)
     resposta = db.Column(db.String(50), nullable=False)
 
